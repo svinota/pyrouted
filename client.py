dc725b11a2892b36f73cd332d29f5dc205732e17
Peter V. Saveliev <peter@svinota.eu>
2014-09-04 19:49:44 +0200

    pyrouted: the simplest JSON rpc approach, one-shot run

---
+++ client.py
@@ -0,0 +1,18 @@
+import sys
+import json
+from pprint import pprint
+from socket import AF_UNIX
+from socket import SOCK_DGRAM
+from socket import socket
+
+local = b'\x00bala'
+remote = b'\x00pyrouted'
+
+call = {'name': 'get_link',
+        'endpoint': local,
+        'argv': sys.argv[1:]}
+s = socket(AF_UNIX, SOCK_DGRAM, 0)
+s.bind(local)
+s.connect(remote)
+s.sendto(json.dumps(call), remote)
+pprint(json.loads(s.recv(16384)))
