c717f98c048e9d3c219afcb83a500d8e3ab3db9d
Peter V. Saveliev <peter@svinota.eu>
2014-09-06 17:30:59 +0200

    pyrouted: use systemd socket activation
    + use netlink for the transport layer

--- client.py
+++ client.py
@@ -1,17 +1,37 @@
+import io
 import sys
 import json
+import struct
 from pprint import pprint
 from socket import AF_UNIX
-from socket import SOCK_DGRAM
+from socket import SOCK_STREAM
 from socket import socket
+from pyroute2.netlink import NLMSG_TRANSPORT
+from pyroute2.netlink import Marshal
+from pyroute2.netlink.generic import cdatamsg
 
 local = b'\x00bala'
 remote = b'\x00pyrouted'
 
 call = {'name': 'get_link',
         'argv': sys.argv[1:]}
-s = socket(AF_UNIX, SOCK_DGRAM, 0)
+s = socket(AF_UNIX, SOCK_STREAM, 0)
 s.bind(local)
 s.connect(remote)
-s.sendto(json.dumps(call), remote)
-pprint(json.loads(s.recv(16384)))
+
+msg = cdatamsg()
+msg['header']['type'] = NLMSG_TRANSPORT
+msg['attrs'] = [['IPR_ATTR_CDATA', json.dumps(call)]]
+msg.encode()
+s.send(msg.buf.getvalue())
+
+marshal = Marshal()
+marshal.msg_map = {NLMSG_TRANSPORT: cdatamsg}
+
+data = io.BytesIO()
+data.length = data.write(s.recv(4))
+length = struct.unpack('I', data.getvalue())[0]
+data.length += data.write(s.recv(length - 4))
+msg = marshal.parse(data)[0]
+
+pprint(json.loads(msg.get_attr('IPR_ATTR_CDATA')))
