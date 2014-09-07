b2565dba46d43154c548e83b02ede2bbed0d8420
Peter V. Saveliev <peter@svinota.eu>
2014-09-07 20:06:53 +0200

    pyrouted: isolate commands module
    + add more comments

--- pyrouted/__init__.py
+++ pyrouted/__init__.py
@@ -1,9 +1,13 @@
 import io
 import json
 import struct
+from pyrouted.commands import Namespace
 from pyroute2.netlink import NLMSG_TRANSPORT
-from pyroute2.netlink import Marshal
+from pyroute2.netlink import Marshal as NetlinkMarshal
 from pyroute2.netlink.generic import cdatamsg
+
+# re-export commands namespace (temporarily)
+Commands = Namespace
 
 
 ##
@@ -16,6 +20,9 @@ def default_encoder(obj):
         return str(type(obj))
 
 
+##
+# main transport class
+#
 class Transport(object):
     '''
     Pyrouted transport protocol. It consists of two
@@ -25,20 +32,37 @@ class Transport(object):
 
     Right now it is JSON serialisation packet into
     netlink, but could be anything else as well.
+
+    Transport object should be instantiated with an
+    open SOCK_STREAM client connection. It doesn't
+    close the socket upon the object destruction, it
+    has nothing to do with fd management. Just a
+    protocol implementation.
     '''
 
-    def __init__(self, sock):
+    def __init__(self, sock, marshal=json):
         self.sock = sock
-        self.marshal = Marshal()
-        self.marshal.msg_map = {NLMSG_TRANSPORT: cdatamsg}
+        self.marshal = marshal
+        self.netlink = NetlinkMarshal()
+        self.netlink.msg_map = {NLMSG_TRANSPORT: cdatamsg}
 
     def loads(self, data):
-        return json.loads(data)
+        '''
+        Load marshalled data from a string.
+        '''
+        return self.marshal.loads(data)
 
     def dumps(self, data):
-        return json.dumps(data, default=default_encoder)
+        '''
+        Dumps a structure to a string.
+        '''
+        return self.marshal.dumps(data, default=default_encoder)
 
     def get(self):
+        '''
+        Get the next message from a stream and return
+        incapsulated data.
+        '''
         # prepare I/O buffer
         data = io.BytesIO()
         # get the message length
@@ -47,11 +71,14 @@ class Transport(object):
         # get the message
         data.length += data.write(self.sock.recv(length - 4))
         # parse the netlink layer -- there will be only one message
-        msg = self.marshal.parse(data)[0]
+        msg = self.netlink.parse(data)[0]
         # get the data
         return self.loads(msg.get_attr('IPR_ATTR_CDATA'))
 
     def put(self, data):
+        '''
+        Marshal provided data end send it to the stream.
+        '''
         # prepare the message
         msg = cdatamsg()
         # set the message type
---
+++ pyrouted/commands.py
@@ -0,0 +1,62 @@
+import os
+from pyroute2 import IPDB
+from types import MethodType
+
+
+##
+# namespace
+#
+class Namespace(object):
+    '''
+    The command namespace -- the main container of all
+    pyrouted commands.
+    '''
+    def __init__(self):
+        self.ipdb = IPDB()
+
+    def __call__(self, call):
+        return getattr(self, call['name'])(*call.get('argv', []),
+                                           **call.get('kwarg', {}))
+
+    def _iscmd(self, command):
+        return not command.startswith('_') and \
+            isinstance(getattr(self, command), MethodType)
+
+    def list(self):
+        '''
+        Return the list of all implemented commands.
+        '''
+        return [x for x in dir(self) if self._iscmd(x)]
+
+    def help(self, command='all'):
+        '''
+        Return the docstring of the requested command.
+        If `command == 'all'`, then return help for all
+        implemented commands.
+        '''
+        ret = []
+        if command == 'all':
+            for c in dir(self):
+                if self._iscmd(c):
+                    ret.append(self.help(c))
+        else:
+            ret = (command, getattr(self, command).__doc__)
+        return ret
+
+    def get_env(self):
+        '''
+        Return the program environment.
+        '''
+        return dict(os.environ)
+
+    def get_links(self):
+        '''
+        Get all the links.
+        '''
+        return list(self.ipdb.by_index.values())
+
+    def get_link(self, name='lo'):
+        '''
+        Get one link.
+        '''
+        return self.ipdb.interfaces[name]
