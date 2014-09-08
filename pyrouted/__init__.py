e79f1b6fc80e5c51d66b8067c850727a68c79d19
Peter V. Saveliev <peter@svinota.eu>
2014-09-08 19:39:01 +0200

    pyrouted: support HTTP and Netlink as transport

--- pyrouted/__init__.py
+++ pyrouted/__init__.py
@@ -1,10 +1,16 @@
 import io
 import json
 import struct
+from socket import MSG_PEEK
 from pyrouted.commands import Namespace
 from pyroute2.netlink import NLMSG_TRANSPORT
 from pyroute2.netlink import Marshal as NetlinkMarshal
 from pyroute2.netlink.generic import cdatamsg
+
+try:
+    from http.client import parse_headers as Headers
+except ImportError:
+    from mimetools import Message as Headers
 
 # re-export commands namespace (temporarily)
 Commands = Namespace
@@ -42,9 +48,16 @@ class Transport(object):
 
     def __init__(self, sock, marshal=json):
         self.sock = sock
+        self.proto = 'netlink'
+        self.rfile = sock.makefile('rb')
+        self.wfile = sock.makefile('wb')
         self.marshal = marshal
         self.netlink = NetlinkMarshal()
         self.netlink.msg_map = {NLMSG_TRANSPORT: cdatamsg}
+
+    def release(self):
+        self.rfile.close()
+        self.wfile.close()
 
     def loads(self, data):
         '''
@@ -65,26 +78,68 @@ class Transport(object):
         '''
         # prepare I/O buffer
         data = io.BytesIO()
-        # get the message length
-        data.length = data.write(self.sock.recv(4))
-        length = struct.unpack('I', data.getvalue())[0]
-        # get the message
-        data.length += data.write(self.sock.recv(length - 4))
-        # parse the netlink layer -- there will be only one message
-        msg = self.netlink.parse(data)[0]
-        # get the data
-        return self.loads(msg.get_attr('IPR_ATTR_CDATA'))
+        # get the message length or type
+        init = self.sock.recv(4, MSG_PEEK)
+        # POST -- request, server side
+        # HTTP -- response, client side
+        if str(init) in ('POST', 'HTTP'):
+            # set proto -- ignored by the client
+            self.proto = 'http_server'
+            # get & dismiss the request line
+            self.rfile.readline()
+            # parse headers
+            headers = Headers(self.rfile)
+            # get the length
+            length = int(headers.getheader('content-length', 0))
+            # read the data
+            data = self.rfile.read(length)
+        else:
+            # set proto
+            self.proto = 'netlink'
+            # get the length
+            length = struct.unpack('I', init)[0]
+            # get the message
+            data.length = data.write(self.sock.recv(length))
+            # parse the netlink layer -- there will be only one message
+            msg = self.netlink.parse(data)[0]
+            # get the data
+            data = msg.get_attr('IPR_ATTR_CDATA')
+        return self.loads(data)
 
     def put(self, data):
         '''
         Marshal provided data end send it to the stream.
         '''
-        # prepare the message
-        msg = cdatamsg()
-        # set the message type
-        msg['header']['type'] = NLMSG_TRANSPORT
-        # incapsulate the data
-        msg['attrs'] = [['IPR_ATTR_CDATA', self.dumps(data)]]
-        # encode and send
-        msg.encode()
-        return self.sock.send(msg.buf.getvalue())
+        data = self.dumps(data)
+        if self.proto.startswith('http'):
+            # client side
+            if self.proto == 'http':
+                # send request
+                self.wfile.write(b'POST / HTTP/1.0\n')
+                self.wfile.write(b'User-Agent: pyroute-client\n')
+            elif self.proto == 'http_server':
+                # send response
+                self.wfile.write(b'HTTP/1.0 200 OK\n')
+                self.wfile.write(b'Server: pyrouted\n')
+            # common headers
+            self.wfile.write(b'Connection: Close\n')
+            self.wfile.write(b'Content-Type: application/json\n')
+            self.wfile.write(b'Content-Length: %i\n\n' % (len(data)))
+            # send the data
+            ret = self.wfile.write(data)
+            # don't forget to flush the data!
+            self.wfile.flush()
+            # FIXME: return len with headers!
+            # bytes sent -- only data
+            return ret
+        elif self.proto == 'netlink':
+            # prepare the message
+            msg = cdatamsg()
+            # set the message type
+            msg['header']['type'] = NLMSG_TRANSPORT
+            # incapsulate the data
+            msg['attrs'] = [['IPR_ATTR_CDATA', data]]
+            # encode and send
+            msg.encode()
+            # bytes sent
+            return self.sock.send(msg.buf.getvalue())
